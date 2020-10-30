a5bb10fc72cf21590bb41b4c2e7b74dfc18ae518
Roma <krab9887@gmail.com>
2020-10-30 15:24:07 +0200

    lab3

--- lab3/dist/css/index.js
+++ lab3/dist/css/index.js
@@ -1,4 +1,31 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (performance.navigation.type == 1) {
+      if(checkCookie('maxNumber')){
+        if(window.confirm(`Some Cookies have been created on this page: \n${document.cookie} \n Do you want to save them?`)) 
+        alert('Cookies were saved, now the page will be refreshed.');
+        else{
+            setCookie('maxNumber', '');
+        } 
+      }
+    }
+    setCookie('session', parseInt(getCookie('session')) + 1, 1);
+    if(checkCookie('maxNumber')) document.querySelector('#numForm').remove();
+    })
+    
+    const setCookie = (name, data, expDays) => {
+     const d = new Date();
+     d.setDate(d.getDate() + expDays);
+     document.cookie = `${name}=${data};expires=${d.toUTCString()};path=/`;
+    }
+    
+    const checkCookie = (name) => {
+     return (document.cookie.includes(name) && !document.cookie.includes(`${name}=;`));
+    }
+    
+    const getCookie = (name) => {
+      return checkCookie(name) ? document.cookie.split(';').find((c) => c.includes(name)).split('=')[1] : 0;
+    }
 
 // 1
 let tmp = document.querySelector('#section-5').innerHTML;
@@ -11,21 +38,23 @@ let a = 3, b = 4, c = 5, p = (a + b + c)/2;
 let square = Math.sqrt(p*(p-a)*(p-b)*(p-c));
 thirdSect.append('Square - ' + square + ' sm^2');
 // 3
-function clickMe() {
+document.querySelector('#numBtn').addEventListener('click', () => {
     let first = document.querySelector('#input1').value;
-    let second = document.querySelector('#input2').value;
-    let third = document.querySelector('#input3').value;
-    let forth = document.querySelector('#input4').value;
-    let fifth = document.querySelector('#input5').value;
-    let sixth = document.querySelector('#input6').value;
-    let seventh = document.querySelector('#input7').value;
-    let eighth = document.querySelector('#input8').value;
-    let nineth = document.querySelector('#input9').value;
-    let tenth = document.querySelector('#input10').value;
-    
-    let mass = [first, second, third, forth, fifth, sixth, seventh, eighth, nineth, tenth];
-    alert('The biggest number is ' + Math.max(...mass));
-}
+      let second = document.querySelector('#input2').value;
+      let third = document.querySelector('#input3').value;
+      let forth = document.querySelector('#input4').value;
+      let fifth = document.querySelector('#input5').value;
+      let sixth = document.querySelector('#input6').value;
+      let seventh = document.querySelector('#input7').value;
+      let eighth = document.querySelector('#input8').value;
+      let nineth = document.querySelector('#input9').value;
+      let tenth = document.querySelector('#input10').value;
+      let mass = [first, second, third, forth, fifth, sixth, seventh, eighth, nineth, tenth];
+      let max = Math.max(...mass);
+      let min = Math.min(...mass);
+      alert('The biggest number is ' + max);
+      setCookie('maxNumber', max,2);
+  })
 
 // 4
 if(window.localStorage)
